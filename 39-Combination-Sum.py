49520241fa31b8e55b7ec99b587615907c18ca54
RamSiddesh <73747305+RamSiddesh@users.noreply.github.com>
2026-09-01 11:37:45 +0530

    [39] [Time Beats: 21.26%] [Memory Beats: 34.24%] - LeetPush

---
+++ 39-Combination-Sum.py
@@ -0,0 +1,26 @@
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        res = []
+
+        def combsum(start,comb,total):
+            if sum(comb) == target:
+                res.append(comb[:])
+                return
+
+            if total>target or start >= len(candidates):
+                return
+
+            combsum(start,comb+[candidates[start]],total+candidates[start])
+
+
+            combsum(start+1,comb,total)
+
+            return res
+
+        return combsum(0,[],0)
+
+
+            
+
+
+        
